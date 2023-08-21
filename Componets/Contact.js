97533b3611cc5dbd7f37d30d203b3b1f78ab8e34
etcspl <mail.etcspl@gmail.com>
2023-08-21 12:29:27 +0530

    changes

---
+++ Componets/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View,Text,StyleSheet} from "react-native";
+
+const App = () => {
+    return <View style= {styles.mainContainer}>
+        <Text style= {styles.mainContainer}>Manjeet Cotton </Text>
+        <Text style={styles.discription}>Login Form</Text>
+        <View style={styles.inputContainer}>
+            
+
+        </View>
+    </View>
+    
+}
